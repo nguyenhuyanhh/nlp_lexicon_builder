2f2cbe25f483954bfa60fd4bf570689343077817
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-11-15 17:51:22 +0800

    split_json, change_data, combine

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import wave
+from decimal import Decimal
 
 import sox
 
@@ -12,16 +13,17 @@ AUDIO_DIR = os.path.join(DOWNLOADER_DIR, 'download/')
 
 def get_duration(path):
     with wave.open(path, 'r') as f:
-        return f.getnframes() / float(f.getframerate())
+        return Decimal(f.getnframes()) / f.getframerate()
 
 
 def mp3_to_wav(path):
     dir, file = os.path.split(path)
     new_file = os.path.splitext(file)[0] + '.wav'
     new_path = os.path.join(dir, new_file)
-    tfm = sox.Transformer()
-    tfm.convert(samplerate=16000, n_channels=1, bitdepth=16)
-    tfm.build(path, new_path)
+    if not os.path.exists(new_path):
+        tfm = sox.Transformer()
+        tfm.convert(samplerate=16000, n_channels=1, bitdepth=16)
+        tfm.build(path, new_path)
     return new_path
 
 
@@ -34,16 +36,23 @@ def main(path):
         if os.path.splitext(file)[1] == '.mp3':
             cur_path = os.path.join(path, file)
             new_path = mp3_to_wav(cur_path)
-            file_list[os.path.splitext(file)[0]] = get_duration(new_path)
+            file_list[os.path.splitext(file)[0]] = str(get_duration(new_path))
             wav_list[os.path.splitext(file)[0]] = new_path
 
+    with wave.open('a.wav', 'w') as w:
+        w.setnchannels(1)
+        w.setsampwidth(2)
+        w.setframerate(16000)
+
     cbm = sox.Combiner()
-    cbm_list = [wav_list[f] for f in sorted(wav_list.keys())]
-    cbm.build(cbm_list, 'a.wav', 'concatenate')
+    for file in sorted(wav_list.keys())[:50]:
+        os.rename('a.wav', 'a_temp.wav')
+        cbm_list = ['a_temp.wav', wav_list[file]]
+        cbm.build(cbm_list, 'a.wav', 'concatenate')
 
     for file in sorted(file_list.keys()):
-        duration = file_list[file]
-        file_list[file] = start_time, start_time + duration
+        duration = Decimal(file_list[file])
+        file_list[file] = str(start_time), str(start_time + duration)
         start_time += duration
 
     with open('out.json', 'w') as w:
--- json_ops.py
+++ json_ops.py
@@ -1,26 +1,41 @@
 import json
 import os
+import shutil
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 DOWNLOADER_DIR = os.path.join(CUR_DIR, 'downloader/')
 
 
 def clean_json():
-    pattern_fail = 'http://www.oxforddictionaries.com/'
+    fails = ['www.oxforddictionaries.com', 'www.onelook.com']
     with open(os.path.join(DOWNLOADER_DIR, 'ultimate.json'), 'r') as f:
         ultimate_list = json.load(f)
     clean_list = dict()
     for word, links in ultimate_list.items():
         for link in links:
-            if link.find(pattern_fail) < 0:
+            if (link.find(fails[0]) < 0 and link.find(fails[1]) < 0):
                 clean_list[word] = link
                 break
     with open(os.path.join(DOWNLOADER_DIR, 'clean.json'), 'w') as w:
         json.dump(clean_list, w, sort_keys=True, indent=4)
 
 
-def split_json():
-    pass
+def split_json(char):
+    with open(os.path.join(DOWNLOADER_DIR, 'clean.json'), 'r') as f:
+        clean_list = json.load(f)
+    split_list = dict()
+    for word, link in clean_list.items():
+        if word[0] == char:
+            split_list[word] = link
+    with open(os.path.join(DOWNLOADER_DIR, 'clean_{}.json'.format(char)), 'w') as w:
+        json.dump(split_list, w, sort_keys=True, indent=4)
+
+
+def change_data_json(path):
+    os.remove(os.path.join(DOWNLOADER_DIR, 'data.json'))
+    shutil.copy(path, os.path.join(DOWNLOADER_DIR, 'data.json'))
 
 if __name__ == '__main__':
     clean_json()
+    split_json('a')
+    change_data_json(os.path.join(DOWNLOADER_DIR, 'clean_a.json'))
