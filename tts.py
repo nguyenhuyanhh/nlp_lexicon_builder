e35dcbe938a6e7d728801a6f14fd64de97096c20
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-01-03 11:04:46 +0800

    tts: pronounce, wrap operations in a class

--- tts.py
+++ tts.py
@@ -13,22 +13,50 @@ if not os.path.exists(TTS_DIR):
     os.makedirs(TTS_DIR)
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger().disabled = True
 LOG = logging.getLogger(__name__)
 
 
-def load_folder(path_):
-    """Load lexicon information from a folder."""
-    lexicon = dict()
-    for file_ in os.listdir(path_):
-        if os.path.splitext(file_)[1] == '.json':
-            index = os.path.splitext(file_)[0]
-            with open(os.path.join(path_, file_), 'r') as lookup_:
-                lookup = json.load(lookup_)
-            lexicon[index] = lookup
-    with open(os.path.join(TTS_DIR, 'lexicon.json'), 'w') as file_out:
-        json.dump(lexicon, file_out, sort_keys=True, indent=4)
-    LOG.info('Loaded %s.', path_)
-    return lexicon
+class Tts():
+    """
+    Class for TTS operations using a lexicon folder.
+    Syntax: Tts(path_)
+    """
+
+    def __init__(self, path_):
+        """Load lexicon information from a folder."""
+        self.lexpath = os.path.abspath(path_)
+        lexicon = dict()
+        for file_ in os.listdir(self.lexpath):
+            if os.path.splitext(file_)[1] == '.json':
+                index = os.path.splitext(file_)[0]
+                with open(os.path.join(self.lexpath, file_), 'r') as lookup_:
+                    lookup = json.load(lookup_)
+                lexicon[index] = lookup
+        self.lexicon = lexicon
+        LOG.info('Loaded %s.', path_)
+
+    def pronounce(self, word):
+        """Extract pronunciation of a word to wav."""
+        index = word[0]
+        lookup = self.lexicon[index]
+        try:
+            start_time = Decimal(lookup[word][0])
+            end_time = Decimal(lookup[word][1])
+            wav_file = os.path.join(self.lexpath, '{}.wav'.format(index))
+            out_file = os.path.join(TTS_DIR, '{}.wav'.format(word))
+            tfm = sox.Transformer()
+            tfm.trim(start_time, end_time)
+            tfm.pad(end_duration=0.1)
+            tfm.build(wav_file, out_file)
+            LOG.info('Processed %s', word)
+            return out_file
+        except KeyError:
+            LOG.info('Not found: %s', word)
+            return None
+
 
 if __name__ == '__main__':
-    load_folder(os.path.join(CUR_DIR, 'build_output/'))
+    TTS = Tts(os.path.join(CUR_DIR, 'build_output/'))
+    TTS.pronounce('apple')
+    TTS.pronounce('gaygisu')
